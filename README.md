9c7fa6cda728ac81bb5de1c2e4d58e30fb02ea40
leonardo <leoalencar4@gmail.com>
2025-09-24 23:29:47 -0300

    adiciona e configura o prettier como dependiencia do projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 projeto de implementação do tabnews.com.br para o curso.dev, desenvolvendo uma aplicação completa passando por todas as etapas do processo de desenvolvilmento.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-function home(){
-
-    return <h1>Aprendi a fazer deploy na vercel!!!!</h1>
-
+function home() {
+  return <h1>Aprendi a fazer deploy na vercel!!!!</h1>;
 }
-
 export default home;
