e843630a2ad3b16e1c3a9757abfc0d7e39472776
leonardo <leoalencar4@gmail.com>
2025-12-03 21:57:56 -0300

    incrmeenta um log de erro na execução da api de status

--- infra/database.js
+++ infra/database.js
@@ -11,6 +11,14 @@ const pool = new Pool({
   max: 10,
 });
 
+console.log("credenciais do postgres", {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
 pool.on("error", (err) => {
   console.log("erro inesperado na pool", err);
 });
